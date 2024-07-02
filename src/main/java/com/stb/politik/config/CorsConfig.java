bf9676c7a26444a6457c30fb449d204b201c1a5f
Sergio Terrino <120554805+sergioterrino@users.noreply.github.com>
2024-07-02 14:49:40 +0200

    3 commit - add to cors el frontend desplegado en netlify

--- src/main/java/com/stb/politik/config/CorsConfig.java
+++ src/main/java/com/stb/politik/config/CorsConfig.java
@@ -24,13 +24,13 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry
                     .addMapping("/login")
-                    .allowedOrigins("http://localhost:4200")
+                    .allowedOrigins("http://localhost:4200", "https://stb-politik.netlify.app/start")
                     .allowedMethods("*")
                     .exposedHeaders("*");
 
                 registry
                     .addMapping("/api/**")
-                    .allowedOrigins("http://localhost:4200")
+                    .allowedOrigins("http://localhost:4200", "https://stb-politik.netlify.app/start")
                     .allowedMethods("*")
                     .exposedHeaders("*");
                 }
